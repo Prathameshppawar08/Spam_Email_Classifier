b9d829a4a4005a9f098642eeb1d848ebc09371dd
Prathameshppawar08 <prathameshppawar88@gmail.com>
2025-03-06 00:31:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,31 @@
 import streamlit as st
 import pickle
 import string
-from nltk.corpus import stopwords
 import nltk
-nltk.download('punkt')
-nltk.download('popular')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 from nltk.stem.porter import PorterStemmer
+
+# Download NLTK datasets for stopwords and punkt if not already present
+nltk.download('punkt', quiet=True)
+nltk.download('stopwords', quiet=True)
 
 ps = PorterStemmer()
 
-
+# Preprocess text
 def transform_text(text):
-    text = text.lower()
-    text = nltk.word_tokenize(text)
+    text = text.lower()  # Convert to lowercase
+    text = word_tokenize(text)  # Tokenize the text
 
     y = []
     for i in text:
-        if i.isalnum():
+        if i.isalnum():  # Remove non-alphanumeric characters
             y.append(i)
 
     text = y[:]
     y.clear()
 
+    # Remove stopwords and punctuation
     for i in text:
         if i not in stopwords.words('english') and i not in string.punctuation:
             y.append(i)
@@ -29,27 +33,33 @@ def transform_text(text):
     text = y[:]
     y.clear()
 
+    # Apply stemming
     for i in text:
         y.append(ps.stem(i))
 
     return " ".join(y)
 
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
 
+# Load model and vectorizer
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+# Streamlit UI
 st.title("Email/SMS Spam Classifier")
 
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-
-    # 1. preprocess
+    # 1. Preprocess input
     transformed_sms = transform_text(input_sms)
-    # 2. vectorize
+
+    # 2. Vectorize the input message using the loaded TF-IDF vectorizer
     vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
+
+    # 3. Make prediction using the loaded model
     result = model.predict(vector_input)[0]
-    # 4. Display
+
+    # 4. Display prediction result
     if result == 1:
         st.header("Spam")
     else:
